a1a6d058db478fff37d4ecf089991e7848ce3c10
Cezary Juszczak <cjusz@ift.uni.wroc.pl>
2013-10-12 15:04:08 +0200

    POT calculation for ND280 implemented.

--- src/geomy.h
+++ src/geomy.h
@@ -16,7 +16,7 @@
 #include <TFile.h>		//loading geometry file
 #include <TGeometry.h>		//geo
 #include <TGeoManager.h>	
-#include <TGeoBBox.h>		//detector bounding box
+#include <TGeoBBox.h>		//_detector bounding box
 #include <TGeoMedium.h>
 #include <TGeoMaterial.h>
 #include <TGeoElement.h>
@@ -48,6 +48,9 @@ class geomy
 	vec orig, dxyz;
 	double max_density;
 	double max_length;
+	double dens,ndens;
+	double pots;
+	double npots;
 
 	TGeometry* geo;
 	TGeoVolume* top;
@@ -62,7 +65,8 @@ class geomy
 
 public:
 	geomy(std::string _filename, std::string _geoname, std::string volume="", vec _d = vec(0,0,0), vec _o = vec(0,0,0) )
-	{		
+	{		pots=npots=0;
+			dens=ndens=0;
 		//		mtrand.SetSeed(0);  // If seed is 0 (default value) a TUUID is generated and used to fill/*
 								// the first 8 integers of the seed array.
 								// In this case the seed is guaranteed to be unique in space and time.
@@ -79,7 +83,7 @@ public:
 		if(top==NULL)   
 			top = gGeoManager->GetTopVolume();
 		if(top==NULL)
-			throw(" Error making detector");
+			throw(" Error making _detector");
 		sha1 = top->GetShape();
 		if(sha1)
 		{
@@ -98,7 +102,7 @@ public:
 		}
 		
 		if(not( _d.x == 0 || _d.y == 0 || _d.z == 0))
-		{   // find intersection of box of interest with detector geometry
+		{   // find intersection of box of interest with _detector geometry
 			vec a=max(orig-dxyz,_o-_d);
 			vec b=min(orig+dxyz,_o+_d);
 			if(b.x>a.x && b.y>a.y && b.z>a.z)
@@ -120,8 +124,7 @@ public:
 	material getpoint()
 	{
 		vec r =  orig - dxyz + 2*vec(frandom()*dxyz.x,frandom()*dxyz.y,frandom()*dxyz.z);
-		material tam=getpoint(r);
-		return tam;
+		return getpoint(r);
 	}
 
 	material getpoint(vec r)
@@ -157,14 +160,18 @@ public:
 			tam.Z = mat1->GetZ();
 			tam.N = d_round(tam.A) - tam.Z;
 		}
-		tam.w_density = mat1->GetDensity();
+		double CLHEP_g_cm3=6.24151e+18;
+		tam.w_density = mat1->GetDensity()/CLHEP_g_cm3;
+		dens+=tam.w_density;
+		ndens++;
+		max_density = max(max_density,tam.w_density);
 		tam.r = r;
 		return tam;	
 	}
 
 
 	material getpoint(vec dir, vec start)
-	{   static double MaxLen=0;
+	{   
 		double x,dx,y,dy,z,dz;
 	    x=y=z=0;
 	    dx=dy=dz=1e40;
@@ -181,20 +188,61 @@ public:
 		  dz=abs(dxyz.z/dir.z);
 		}
 		material tam;
+		tam.w_density=0;
 
 		double ta=max(x-dx,max(y-dy,z-dz));
 		double tb=min(x+dx,min(y+dy,z+dz));
 		if(tb>ta)
-		 {  double len=dir.length()*(tb-ta);
-		    if(len>frandom()*MaxLen)
+		 {  double len=dir.length()*(tb-ta)*mm/cm; // from mm to cm
+
+		    if(len>frandom()*max_length)
 		      {
-			    if(len>MaxLen) 
-		            MaxLen=len;
-			   tam = getpoint(start+ (ta+frandom()*(tb-ta))*dir);
+			    if(len>max_length) 
+		            max_length=len;
+			    tam = getpoint(start+ (ta+frandom()*(tb-ta))*dir);
 		      }
-		    // else tam.density==0 and event will be rejected  
 		  }
+		double mol=6.02214129e23 ;
+		/* density is in g/cm3 
+		*  length is in cm
+		*  mol = number of nucleons per gram
+		*  pots is in nucleons/cm2
+		*/ 
+		pots+=tam.w_density*max_length*mol;
+		npots++;  
+		//~ cout<<tam.w_density/CLHEP_g_cm3<<'\t';
+		//~ cout<<tam.A<<'\t';
+		//~ cout<<tam.Z<<'\t';
+		//~ 
+		//~ cout<<max_density/CLHEP_g_cm3<<'\t';
+		//~ cout<<max_length/10<<'\t';
+		//~ cout<<nucleons_per_cm2()<<endl;
 		return tam;
+	}
+	
+	double max_len()
+	{
+		return max_length;
+	}
+
+	double max_dens()
+	{
+		return max_density;
+	}
+	
+	double nucleons_per_cm2()
+	{
+		return npots>0 ? pots/npots : 0;
+	}
+	
+	double density()
+	{
+		return dens/ndens*gram/cm3; 
+	}
+	
+	double vol_mass()
+	{
+		return 8*dxyz.x*dxyz.y*dxyz.z*mm3*density(); 
 	}
 
 	~geomy()
