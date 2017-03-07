159931ed02e5ac9750ade476458a94788b6fc753
Kajetan Niewczas <kajetan.niewczas@uwr.edu.pl>
2017-03-07 21:02:26 +0900

    [NoCode] small clean-up

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,15 @@
 #include"nuwro.h"
 
-	NuWro nuwro;
+NuWro nuwro;
 
 #include "tests.h"
 int main(int argc, char** argv)
 {
 
-//	test_ort();return 0;
-//	testsigma();return 0;
+//  test_ort();return 0;
+//  testsigma();return 0;
 //  test_boost();return 0;
-	nuwro.main(argc,argv);
-    return 0;
+
+  nuwro.main(argc,argv);
+  return 0;
 }
