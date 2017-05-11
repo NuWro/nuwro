7f31b9a86215953f340c14d968cfe55ac8e5159a
Kajetan Niewczas <kajetan.niewczas@uwr.edu.pl>
2017-05-11 15:35:26 +0200

    New class created that will be responsible for handling the input data.

---
+++ src/input_data.cc
@@ -0,0 +1,2 @@
+#include "input_data.h"
+
---
+++ src/input_data.h
@@ -0,0 +1,21 @@
+#ifndef _INPUT_DATA_h_
+#define _INPUT_DATA_h_
+
+
+//! Manager of the input data needed in NuWro.
+/*! This class decides which data files should be read, reads them and stores the data */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
