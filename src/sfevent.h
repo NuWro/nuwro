1a1998f36cc01da06b69e462872cde444ff9c2ee
Tomasz Golan <tomasz.golan@gmail.com>
2017-05-26 11:22:21 +0200

    [NoCode] doxygen comments added in SF header + formatting

--- src/sfevent.h
+++ src/sfevent.h
@@ -1,13 +1,18 @@
 #ifndef _sfevent_h_
 #define _sfevent_h_
-#include "params.h"
 #include "event1.h"
 #include "nucleus.h"
+#include "params.h"
 
+//! check if SF exists for a given nucleus (FG will be used if False)
 bool has_sf(nucleus &t, int method);
 
-double sfevent(params &p,event &e,nucleus &t);
-double sfevent2cc(params &p,event &e,nucleus &t);
-double sfevent2nc(params &p,event &e,nucleus &t);
+//! generate kinematics and calculate cross section for CC QEL event using SF
+double sfevent2cc(params &p, event &e, nucleus &t);
+//! generate kinematics and calculate cross section for NC EL event using SF
+double sfevent2nc(params &p, event &e, nucleus &t);
+
+//! TODO: not yet defined; placeholder for new implementation (for both CC and NC)
+double sfevent(params &p, event &e, nucleus &t);
 
 #endif
