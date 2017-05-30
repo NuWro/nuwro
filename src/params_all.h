cdd17ba93ace8fc7ed5c24433e9e326111c00721
Tomasz Golan <tomasz.golan@gmail.com>
2017-05-30 12:10:34 +0200

    [new param] sf_fsi to turn on/off fsi for sf

--- src/params_all.h
+++ src/params_all.h
@@ -61,6 +61,7 @@ PARAM(double,qel_nc_axial_mass,1350)\
 PARAM(double,qel_s_axial_mass,1200)\
 PARAM(bool,flux_correction,1)\
 PARAM(int,sf_method,0)\
+PARAM(bool,sf_fsi,0)\
 PARAM(int,sf_pb,0)\
 PARAM(bool,cc_smoothing,1)\
 PARAM(int,delta_FF_set,1)\
