55da56620669abb733ef2949fc86a665ae50f7bf
Tomasz Golan <tomasz.golan@gmail.com>
2017-10-09 19:05:13 +0200

    more understandable variables names

--- src/rew/reweight_to.cc
+++ src/rew/reweight_to.cc
@@ -34,46 +34,46 @@ void not_reweightable(const string par) {
 }
 
 enum {
-  both = 0,  // create both outputs (default)
-  events,    // create only file with weighted events
-  weights    // create only file with weights
+  both = 0,    // create both outputs (default)
+  no_weights,  // create only file with weighted events
+  no_events    // create only file with weights
 } mode;
 
 int main(int argc, char* argv[]) {
-  // at least input and output files and on parameter to reweight is required
-  if (argc < 7) usage();
+  // at least input and one parameter to reweight is required
+  if (argc < 5) usage();
 
   // setup input tree
-  TFile* f1 = new TFile(argv[1]);
-  if (f1->IsZombie()) {
+  TFile* tfile_input = new TFile(argv[1]);
+  if (tfile_input->IsZombie()) {
     std::cout << "[ERROR] Couldn't open input file: " << argv[1] << std::endl;
     exit(-1);
   }
 
   // load events tree
-  TTree* t1 = dynamic_cast<TTree*>(f1->Get("treeout"));
-  if (!t1) {
+  TTree* ttree_input = dynamic_cast<TTree*>(tfile_input->Get("treeout"));
+  if (!ttree_input) {
     std::cerr << "[ERROR]: Couldn't find TTree (\"treeout\") in file " << argv[1] << "." << std::endl;
     exit(-1);
   }
 
   // set up event pointer
   event* e = new event;
-  t1->SetBranchAddress("e", &e);
+  ttree_input->SetBranchAddress("e", &e);
 
-  vector<RewParam*> args;    // the list of parameters to reweight
-  vector<double> vals;       // the list of new parameters values
-  char* outname = NULL;      // the full NuWro output file with new weight
-  char* weightsname = NULL;  // the output ROOT file with weights only
+  vector<RewParam*> args;      // the list of parameters to reweight
+  vector<double> vals;         // the list of new parameters values
+  string out_reweighted = "";  // the full NuWro output file with new weight
+  string out_weights = "";     // the output ROOT file with weights only
 
   for (int i = 2; i < argc; i++) {
     // parse command line arguments
     if (string(argv[i]) == "-o")
-      outname = argv[++i];
+      out_reweighted = argv[++i];
     else if (string(argv[i]) == "--no_events")
-      mode = weights;
+      mode = no_events;
     else if (string(argv[i]) == "--no_weights")
-      mode = events;
+      mode = no_weights;
     else if (string(argv[i]) == "-p") {
       // check if parameter is given after -p
       if (not argv[i + 1]) error("Parameter name is required after -p");
@@ -107,71 +107,88 @@ int main(int argc, char* argv[]) {
   if (args.size() == 0) error("Provide at least one parameter to reweight");
 
   // if output filename is not given - use input filename with .reweighted suffix
-  if (outname == NULL) outname = &(string(argv[1]) + ".reweighted")[0];
+  if (out_reweighted == "") out_reweighted = string(argv[1]) + ".reweighted";
 
   // use .weights suffix for a file with weights only
-  weightsname = &(string(outname) + ".weights")[0];
+  out_weights = out_reweighted + ".weights";
 
-  /// create output files
-  TFile* f2 = NULL;
-  TTree* t2 = NULL;
-  TFile* f3 = NULL;
-  TTree* t3 = NULL;
+  // prepare outputs
+  TFile* tfile_weights = NULL;  // just weights
+  TTree* ttree_weights = NULL;
+  TFile* tfile_reweighted = NULL;  // reweighted events
+  TTree* ttree_reweighted = NULL;
 
   double weight;  // weights holder
 
-  if (mode != events) {
-    f2 = new TFile(weightsname, "recreate");
-    t2 = new TTree("weights", "Tree of weights");
-    t2->Branch("weight", &weight, "weight/D");
+  if (mode != no_weights) {
+    tfile_weights = new TFile(out_weights.c_str(), "recreate");
+    ttree_weights = new TTree("weights", "Tree of weights");
+    ttree_weights->Branch("weight", &weight, "weight/D");
   }
 
-  if (mode != weights) {
-    f3 = new TFile(outname, "recreate");
-    t3 = new TTree("treeout", "Tree of events");
-    t3->Branch("e", "event", &e);
+  if (mode != no_events) {
+    tfile_reweighted = new TFile(out_reweighted.c_str(), "recreate");
+    ttree_reweighted = new TTree("treeout", "Tree of events");
+    ttree_reweighted->Branch("e", "event", &e);
   }
 
   // Calculate and save weights for each event
-  int n = t1->GetEntries();
+  int n_events = ttree_input->GetEntries();
 
-  for (int ie = 0; ie < n; ie++) {
-    t1->GetEntry(ie);
+  for (int ie = 0; ie < n_events; ie++) {
+    // get i-th event from the ttree
+    ttree_input->GetEntry(ie);
+
+    // initialize reweighters with event parameters
     REW.init(e->par);
     nucleus t(e->par);
+
+    // call single pion production setup for first event
+    // TODO: disable if pion production is not considered
     if (ie == 0) SetupSPP(e->par);
 
+    // calculate cross section for nominal parameters
     double nominal = REW.weight(*e, e->par, t);
 
+    // change all parameters set up to reweight
     for (int j = 0; j < args.size(); j++) args[j]->set(vals[j]);
 
+    // update form factors
     ff_configure(e->par);
 
+    // calculate cross section with new parameters
     weight = REW.weight(*e, e->par, t) / nominal;
 
-    cout << weight << " ";  // endl;
-    if (t2) t2->Fill();
+    if (ttree_weights) ttree_weights->Fill();
 
-    if (t3) {
+    if (ttree_reweighted) {
       e->weight *= weight;
-      t3->Fill();
+      ttree_reweighted->Fill();
     }
+
+    cout << "Reweighting \"" << argv[1] << "\": " << 100 * ie / n_events << "%\r" << flush;
   }
 
-  f1->Close();
-  delete f1;
-  if (f2) {
-    f2->Write();
-    f2->Close();
+  tfile_input->Close();
+
+  if (tfile_weights) {
+    tfile_weights->Write();
+    tfile_weights->Close();
   }
-  if (f3) {
-    f3->Write();
-    f3->Close();
+
+  if (tfile_reweighted) {
+    tfile_reweighted->Write();
+    tfile_reweighted->Close();
   }
 
   delete e;
-  delete f2;
-  delete f3;
-  cout << "Output file: \"" << outname << "\"" << endl;
+  delete tfile_input;
+  delete tfile_weights;
+  delete tfile_reweighted;
+
+  if (mode != no_events) cout << "Reweighted events saved in: " << out_reweighted << endl;
+
+  if (mode != no_weights) cout << "Weights saved in: " << out_weights << endl;
+
   return 0;
 }
