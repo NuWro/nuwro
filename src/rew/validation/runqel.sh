511829cb38f33ffdcc00c5a34a3b2c382a6d1500
Tomasz Golan <tomasz.golan@gmail.com>
2017-10-30 09:52:05 +0100

    [validation] decrease #cases

--- src/rew/validation/runqel.sh
+++ src/rew/validation/runqel.sh
@@ -4,58 +4,58 @@
 
 ENERGY_MIN=500
 ENERGY_MAX=1500
-ENERGY_STEP=100
+ENERGY_STEP=500
 
 MA_MIN=800
-MA_MAX=1350
-MA_STEP=50
+MA_MAX=1200
+MA_STEP=200
 
 OUTDIR="simulations/qelma/"
 mkdir -p $OUTDIR
 
 ##### SIMULATIONS ######
 
-# for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
-# do
-#     for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
-#     do
-#         ../../../bin/nuwro -i "parqel.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "qel_cc_axial_mass = $ma" \
-#             -p "qel_nc_axial_mass = $ma" \
-#             -p "dyn_qel_cc = 1" \
-#             -p "dyn_qel_nc = 0" \
-#             -o "${OUTDIR}/numu${energy}_cc_ma${ma}.root" &
+for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
+do
+    for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
+    do
+        ../../../bin/nuwro -i "parqel.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "qel_cc_axial_mass = $ma" \
+            -p "qel_nc_axial_mass = $ma" \
+            -p "dyn_qel_cc = 1" \
+            -p "dyn_qel_nc = 0" \
+            -o "${OUTDIR}/numu${energy}_cc_ma${ma}.root" &
 
-#         ../../../bin/nuwro -i "parqel.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "qel_cc_axial_mass = $ma" \
-#             -p "qel_nc_axial_mass = $ma" \
-#             -p "dyn_qel_cc = 0" \
-#             -p "dyn_qel_nc = 1" \
-#             -o "${OUTDIR}/numu${energy}_nc_ma${ma}.root" &
+        ../../../bin/nuwro -i "parqel.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "qel_cc_axial_mass = $ma" \
+            -p "qel_nc_axial_mass = $ma" \
+            -p "dyn_qel_cc = 0" \
+            -p "dyn_qel_nc = 1" \
+            -o "${OUTDIR}/numu${energy}_nc_ma${ma}.root" &
 
-#         ../../../bin/nuwro -i "parqel.txt" \
-#             -p "beam_particle = -14" \
-#             -p "beam_energy = $energy" \
-#             -p "qel_cc_axial_mass = $ma" \
-#             -p "qel_nc_axial_mass = $ma" \
-#             -p "dyn_qel_cc = 1" \
-#             -p "dyn_qel_nc = 0" \
-#             -o "${OUTDIR}/numubar${energy}_cc_ma${ma}.root" &
+        ../../../bin/nuwro -i "parqel.txt" \
+            -p "beam_particle = -14" \
+            -p "beam_energy = $energy" \
+            -p "qel_cc_axial_mass = $ma" \
+            -p "qel_nc_axial_mass = $ma" \
+            -p "dyn_qel_cc = 1" \
+            -p "dyn_qel_nc = 0" \
+            -o "${OUTDIR}/numubar${energy}_cc_ma${ma}.root" &
 
-#         ../../../bin/nuwro -i "parqel.txt" \
-#             -p "beam_particle = -14" \
-#             -p "beam_energy = $energy" \
-#             -p "qel_cc_axial_mass = $ma" \
-#             -p "qel_nc_axial_mass = $ma" \
-#             -p "dyn_qel_cc = 0" \
-#             -p "dyn_qel_nc = 1" \
-#             -o "${OUTDIR}/numubar${energy}_nc_ma${ma}.root"
-#     done
-# done
+        ../../../bin/nuwro -i "parqel.txt" \
+            -p "beam_particle = -14" \
+            -p "beam_energy = $energy" \
+            -p "qel_cc_axial_mass = $ma" \
+            -p "qel_nc_axial_mass = $ma" \
+            -p "dyn_qel_cc = 0" \
+            -p "dyn_qel_nc = 1" \
+            -o "${OUTDIR}/numubar${energy}_nc_ma${ma}.root"
+    done
+done
 
 ##### REWEIGHTING #####
 
