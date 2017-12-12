94c7381bd53c755c8cace0ffef4ced0962260133
Tomasz Golan <tomasz.golan@gmail.com>
2017-12-12 10:44:07 +0100

    [validation] delta s plot

---
+++ src/rew/validation/qel3.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+whats = ("Q2", "Tk", "ang")
+inpath = "qelma"
+
+with PdfPages("qel_delta_s.pdf") as pdf:
+    for what in whats:
+        plt.figure()
+
+        plt.title("numu, nc, E = 1000 MeV")
+
+        x1, y1 = np.loadtxt("{}/numu_s+0.2_{}.txt".format(inpath, what), delimiter=' ', unpack=True)
+        x2, y2 = np.loadtxt("{}/numu_s-0.2_{}.txt".format(inpath, what), delimiter=' ', unpack=True)
+        x1r, y1r = np.loadtxt("{}/numu_s+0.2r_{}.txt".format(inpath, what), delimiter=' ', unpack=True)
+        x2r, y2r = np.loadtxt("{}/numu_s-0.2r_{}.txt".format(inpath, what), delimiter=' ', unpack=True)
+
+        plt.xlabel(what)
+        plt.ylabel("#events")
+
+        plt.plot(x1, y1, 'r-', label='$\Delta s = 0.2$')
+        plt.plot(x2, y2, 'b-', label='$\Delta s = -0.2$')
+        plt.plot(x1r, y1r, 'r*', label='$\Delta s = 0$ rew to $0.2$')
+        plt.plot(x2r, y2r, 'b*', label='$\Delta s = 0$ rew to $-0.2$')
+
+        plt.legend()
+        pdf.savefig()
