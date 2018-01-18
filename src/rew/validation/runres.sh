d2f8d24b1bfd392594b391695d5b138bd8993033
Tomasz Golan <tomasz.golan@gmail.com>
2018-01-18 10:06:56 +0100

    reweighitng loop in runres

--- src/rew/validation/runres.sh
+++ src/rew/validation/runres.sh
@@ -133,51 +133,140 @@ do
     done
 done
 
-##### REWEIGHTING #####
+##### REWEIGHTING AXIAL MASS #####
 
-# for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
-# do
-#     for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
-#     do
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numu${energy}_cc_ma800.root" \
-#             -o "${OUTDIR}/numu${energy}_cc_rewto_ma${ma}.root" \
-#             -p qel_cc_axial_mass $ma \
-#             -p qel_nc_axial_mass $ma &
+for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
+do
+    for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
+    do
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_cc_ma1000_c5a11.root" \
+            -o "${OUTDIR}/numu${energy}_cc_rewto_ma${ma}_c5a11.root" \
+            -p pion_axial_mass $ma &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numu${energy}_nc_ma800.root" \
-#             -o "${OUTDIR}/numu${energy}_nc_rewto_ma${ma}.root" \
-#             -p qel_cc_axial_mass $ma \
-#             -p qel_nc_axial_mass $ma &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_nc_ma1000_c5a11.root" \
+            -o "${OUTDIR}/numu${energy}_nc_rewto_ma${ma}_c5a11.root" \
+            -p pion_axial_mass $ma &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numubar${energy}_cc_ma800.root" \
-#             -o "${OUTDIR}/numubar${energy}_cc_rewto_ma${ma}.root" \
-#             -p qel_cc_axial_mass $ma \
-#             -p qel_nc_axial_mass $ma &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_cc_ma1000_c5a11.root" \
+            -o "${OUTDIR}/numubar${energy}_cc_rewto_ma${ma}_c5a11.root" \
+            -p pion_axial_mass $ma &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numubar${energy}_nc_ma800.root" \
-#             -o "${OUTDIR}/numubar${energy}_nc_rewto_ma${ma}.root" \
-#             -p qel_cc_axial_mass $ma \
-#             -p qel_nc_axial_mass $ma
-#     done
-# done
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_nc_ma1000_c5a11.root" \
+            -o "${OUTDIR}/numubar${energy}_nc_rewto_ma${ma}_c5a11.root" \
+            -p pion_axial_mass $ma &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_cc_ma1000_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_cc_rewto_ma${ma}_c5a12.root" \
+            -p pion_axial_mass $ma &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_nc_ma1000_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_nc_rewto_ma${ma}_c5a12.root" \
+            -p pion_axial_mass $ma &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_cc_ma1000_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_cc_rewto_ma${ma}_c5a12.root" \
+            -p pion_axial_mass $ma &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_nc_ma1000_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_nc_rewto_ma${ma}_c5a12.root" \
+            -p pion_axial_mass $ma &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_cc_ma1000_c5a13.root" \
+            -o "${OUTDIR}/numu${energy}_cc_rewto_ma${ma}_c5a13.root" \
+            -p pion_axial_mass $ma &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_nc_ma1000_c5a13.root" \
+            -o "${OUTDIR}/numu${energy}_nc_rewto_ma${ma}_c5a13.root" \
+            -p pion_axial_mass $ma &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_cc_ma1000_c5a13.root" \
+            -o "${OUTDIR}/numubar${energy}_cc_rewto_ma${ma}_c5a13.root" \
+            -p pion_axial_mass $ma &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_nc_ma1000_c5a13.root" \
+            -o "${OUTDIR}/numubar${energy}_nc_rewto_ma${ma}_c5a13.root" \
+            -p pion_axial_mass $ma
+    done
+done
+
+##### REWEIGHTING PION C5A #####
+
+for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
+do
+    for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
+    do
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_cc_rewto_ma${ma}_c5a12.root" \
+            -p pion_C5A 1.2 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_nc_rewto_ma${ma}_c5a12.root" \
+            -p pion_C5A 1.2 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_cc_rewto_ma${ma}_c5a12.root" \
+            -p pion_C5A 1.2 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_nc_rewto_ma${ma}_c5a12.root" \
+            -p pion_C5A 1.2 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_cc_rewto_ma${ma}_c5a11.root" \
+            -p pion_C5A 1.1 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_nc_rewto_ma${ma}_c5a11.root" \
+            -p pion_C5A 1.1 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_cc_rewto_ma${ma}_c5a11.root" \
+            -p pion_C5A 1.1 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_nc_rewto_ma${ma}_c5a11.root" \
+            -p pion_C5A 1.1 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_cc_rewto_ma${ma}_c5a13.root" \
+            -p pion_C5A 1.3 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_nc_rewto_ma${ma}_c5a13.root" \
+            -p pion_C5A 1.3 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_cc_rewto_ma${ma}_c5a13.root" \
+            -p pion_C5A 1.3 &
+
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_nc_rewto_ma${ma}_c5a13.root" \
+            -p pion_C5A 1.3
+    done
+done
 
 ###### RUN MACRO ######
-
-# for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
-# do
-#     for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
-#     do
-#         ../../../bin/myroot -b -q 'qel.c("'${OUTDIR}'/numu'${energy}'_cc_ma'${ma}'")' &
-#         ../../../bin/myroot -b -q 'qel.c("'${OUTDIR}'/numu'${energy}'_nc_ma'${ma}'")' &
-#         ../../../bin/myroot -b -q 'qel.c("'${OUTDIR}'/numu'${energy}'_cc_rewto_ma'${ma}'")' &
-#         ../../../bin/myroot -b -q 'qel.c("'${OUTDIR}'/numu'${energy}'_nc_rewto_ma'${ma}'")'
-#         ../../../bin/myroot -b -q 'qel.c("'${OUTDIR}'/numubar'${energy}'_cc_ma'${ma}'")' &
-#         ../../../bin/myroot -b -q 'qel.c("'${OUTDIR}'/numubar'${energy}'_nc_ma'${ma}'")' &
-#         ../../../bin/myroot -b -q 'qel.c("'${OUTDIR}'/numubar'${energy}'_cc_rewto_ma'${ma}'")' &
-#         ../../../bin/myroot -b -q 'qel.c("'${OUTDIR}'/numubar'${energy}'_nc_rewto_ma'${ma}'")'
-#     done
-# done
