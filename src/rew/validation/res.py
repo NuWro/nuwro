f0a3721dd0b41303c8339bfca550535d2fb9bb1b
Tomasz Golan <tomasz.golan@gmail.com>
2018-01-22 10:06:47 +0100

    pion axial mass to gev

--- src/rew/validation/res.py
+++ src/rew/validation/res.py
@@ -10,37 +10,37 @@ dyns = ("cc", "nc")
 
 inpath = "res"
 
-# with PdfPages("rew_val_res_ma.pdf") as pdf:
-#     for nu in nus:
-#         for energy in energies:
-#             for c5a in c5as:
-#                 for dyn in dyns:
-#                     for pion in ("pip", "pi0", "pim"):
-#                         plt.figure()
+with PdfPages("rew_val_res_ma.pdf") as pdf:
+    for nu in nus:
+        for energy in energies:
+            for c5a in c5as:
+                for dyn in dyns:
+                    for pion in ("pip", "pi0", "pim"):
+                        plt.figure()
 
-#                         plt.title("{}, {}, E = {}, C5A = {}, pion = {}".format(nu, dyn, energy, c5a, pion))
+                        plt.title("{}, {}, E = {}, C5A = {}, pion = {}".format(nu, dyn, energy, c5a, pion))
 
-#                         x1a, y1a = np.loadtxt("res/{}{}_{}_ma900_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
-#                         x1b, y1b = np.loadtxt("res/{}{}_{}_ma1000_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
-#                         x1c, y1c = np.loadtxt("res/{}{}_{}_ma1100_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
+                        x1a, y1a = np.loadtxt("res/{}{}_{}_ma900_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
+                        x1b, y1b = np.loadtxt("res/{}{}_{}_ma1000_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
+                        x1c, y1c = np.loadtxt("res/{}{}_{}_ma1100_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
 
-#                         x2a, y2a = np.loadtxt("res/{}{}_{}_marewto_ma900_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
-#                         x2b, y2b = np.loadtxt("res/{}{}_{}_marewto_ma1000_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
-#                         x2c, y2c = np.loadtxt("res/{}{}_{}_marewto_ma1100_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
+                        x2a, y2a = np.loadtxt("res/{}{}_{}_marewto_ma900_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
+                        x2b, y2b = np.loadtxt("res/{}{}_{}_marewto_ma1000_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
+                        x2c, y2c = np.loadtxt("res/{}{}_{}_marewto_ma1100_c5a{}_{}.txt".format(nu, energy, dyn, int(c5a*10), pion), delimiter=' ', unpack=True)
 
-#                         plt.xlabel("pion momentum [GeV]")
-#                         plt.ylabel("arbitrary units   ")
+                        plt.xlabel("pion momentum [GeV]")
+                        plt.ylabel("arbitrary units   ")
 
-#                         plt.plot(x1a, y1a, 'r-', label='Ma = 900')
-#                         plt.plot(x1b, y1b, 'b-', label='Ma = 1000')
-#                         plt.plot(x1c, y1c, 'g-', label='Ma = 1100')
+                        plt.plot(x1a, y1a, 'r-', label='Ma = 900')
+                        plt.plot(x1b, y1b, 'b-', label='Ma = 1000')
+                        plt.plot(x1c, y1c, 'g-', label='Ma = 1100')
 
-#                         plt.plot(x2a, y2a, 'r.', label='1000 -> 900')
-#                         plt.plot(x2b, y2b, 'b.', label='1000 -> 1000')
-#                         plt.plot(x2c, y2c, 'g.', label='1000 -> 1100')
+                        plt.plot(x2a, y2a, 'r.', label='1000 -> 900')
+                        plt.plot(x2b, y2b, 'b.', label='1000 -> 1000')
+                        plt.plot(x2c, y2c, 'g.', label='1000 -> 1100')
 
-#                         plt.legend()
-#                         pdf.savefig()
+                        plt.legend()
+                        pdf.savefig()
 
 with PdfPages("rew_val_res_c5a.pdf") as pdf:
     for nu in nus:
--- src/rew/validation/runres.sh
+++ src/rew/validation/runres.sh
@@ -19,119 +19,121 @@ mkdir -p $OUTDIR
 
 ##### SIMULATIONS ######
 
-# for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
-# do
-#     for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
-#     do
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.1" \
-#             -p "dyn_res_cc = 1" \
-#             -p "dyn_res_nc = 0" \
-#             -o "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a11.root" &
+for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
+do
+    for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
+    do
+        mav=$((ma/1000))
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.1" \
-#             -p "dyn_res_cc = 0" \
-#             -p "dyn_res_nc = 1" \
-#             -o "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a11.root" &
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.1" \
+            -p "dyn_res_cc = 1" \
+            -p "dyn_res_nc = 0" \
+            -o "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a11.root" &
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = -14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.1" \
-#             -p "dyn_res_cc = 1" \
-#             -p "dyn_res_nc = 0" \
-#             -o "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a11.root" &
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.1" \
+            -p "dyn_res_cc = 0" \
+            -p "dyn_res_nc = 1" \
+            -o "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a11.root" &
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.1" \
-#             -p "dyn_res_cc = 0" \
-#             -p "dyn_res_nc = 1" \
-#             -o "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a11.root" &
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = -14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.1" \
+            -p "dyn_res_cc = 1" \
+            -p "dyn_res_nc = 0" \
+            -o "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a11.root" &
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.2" \
-#             -p "dyn_res_cc = 1" \
-#             -p "dyn_res_nc = 0" \
-#             -o "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" &
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.1" \
+            -p "dyn_res_cc = 0" \
+            -p "dyn_res_nc = 1" \
+            -o "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a11.root" &
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.2" \
-#             -p "dyn_res_cc = 0" \
-#             -p "dyn_res_nc = 1" \
-#             -o "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" &
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.2" \
+            -p "dyn_res_cc = 1" \
+            -p "dyn_res_nc = 0" \
+            -o "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" &
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = -14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.2" \
-#             -p "dyn_res_cc = 1" \
-#             -p "dyn_res_nc = 0" \
-#             -o "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" &
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.2" \
+            -p "dyn_res_cc = 0" \
+            -p "dyn_res_nc = 1" \
+            -o "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" &
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.2" \
-#             -p "dyn_res_cc = 0" \
-#             -p "dyn_res_nc = 1" \
-#             -o "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" &
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = -14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.2" \
+            -p "dyn_res_cc = 1" \
+            -p "dyn_res_nc = 0" \
+            -o "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" &
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.3" \
-#             -p "dyn_res_cc = 1" \
-#             -p "dyn_res_nc = 0" \
-#             -o "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a13.root" &
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.2" \
+            -p "dyn_res_cc = 0" \
+            -p "dyn_res_nc = 1" \
+            -o "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" &
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.3" \
-#             -p "dyn_res_cc = 0" \
-#             -p "dyn_res_nc = 1" \
-#             -o "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a13.root" &
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.3" \
+            -p "dyn_res_cc = 1" \
+            -p "dyn_res_nc = 0" \
+            -o "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a13.root" &
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = -14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.3" \
-#             -p "dyn_res_cc = 1" \
-#             -p "dyn_res_nc = 0" \
-#             -o "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a13.root" &
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.3" \
+            -p "dyn_res_cc = 0" \
+            -p "dyn_res_nc = 1" \
+            -o "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a13.root" &
 
-#         ../../../bin/nuwro -i "parres.txt" \
-#             -p "beam_particle = 14" \
-#             -p "beam_energy = $energy" \
-#             -p "pion_axial_mass = $ma" \
-#             -p "pion_C5A = 1.3" \
-#             -p "dyn_res_cc = 0" \
-#             -p "dyn_res_nc = 1" \
-#             -o "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a13.root"
-#     done
-# done
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = -14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.3" \
+            -p "dyn_res_cc = 1" \
+            -p "dyn_res_nc = 0" \
+            -o "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a13.root" &
+
+        ../../../bin/nuwro -i "parres.txt" \
+            -p "beam_particle = 14" \
+            -p "beam_energy = $energy" \
+            -p "pion_axial_mass = $mav" \
+            -p "pion_C5A = 1.3" \
+            -p "dyn_res_cc = 0" \
+            -p "dyn_res_nc = 1" \
+            -o "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a13.root"
+    done
+done
 
 ##### REWEIGHTING AXIAL MASS #####
 
@@ -139,141 +141,143 @@ for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
 do
     for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
     do
+        mav=$((ma/1000))
+    
         ../../../bin/reweight_to \
             "${OUTDIR}/numu${energy}_cc_ma1000_c5a11.root" \
             -o "${OUTDIR}/numu${energy}_cc_marewto_ma${ma}_c5a11.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numu${energy}_nc_ma1000_c5a11.root" \
             -o "${OUTDIR}/numu${energy}_nc_marewto_ma${ma}_c5a11.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numubar${energy}_cc_ma1000_c5a11.root" \
             -o "${OUTDIR}/numubar${energy}_cc_marewto_ma${ma}_c5a11.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numubar${energy}_nc_ma1000_c5a11.root" \
             -o "${OUTDIR}/numubar${energy}_nc_marewto_ma${ma}_c5a11.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numu${energy}_cc_ma1000_c5a12.root" \
             -o "${OUTDIR}/numu${energy}_cc_marewto_ma${ma}_c5a12.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numu${energy}_nc_ma1000_c5a12.root" \
             -o "${OUTDIR}/numu${energy}_nc_marewto_ma${ma}_c5a12.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numubar${energy}_cc_ma1000_c5a12.root" \
             -o "${OUTDIR}/numubar${energy}_cc_marewto_ma${ma}_c5a12.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numubar${energy}_nc_ma1000_c5a12.root" \
             -o "${OUTDIR}/numubar${energy}_nc_marewto_ma${ma}_c5a12.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numu${energy}_cc_ma1000_c5a13.root" \
             -o "${OUTDIR}/numu${energy}_cc_marewto_ma${ma}_c5a13.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numu${energy}_nc_ma1000_c5a13.root" \
             -o "${OUTDIR}/numu${energy}_nc_marewto_ma${ma}_c5a13.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numubar${energy}_cc_ma1000_c5a13.root" \
             -o "${OUTDIR}/numubar${energy}_cc_marewto_ma${ma}_c5a13.root" \
-            -p pion_axial_mass $ma &
+            -p pion_axial_mass $mav &
 
         ../../../bin/reweight_to \
             "${OUTDIR}/numubar${energy}_nc_ma1000_c5a13.root" \
             -o "${OUTDIR}/numubar${energy}_nc_marewto_ma${ma}_c5a13.root" \
-            -p pion_axial_mass $ma
+            -p pion_axial_mass $mav
     done
 done
 
 ##### REWEIGHTING PION C5A #####
 
-# for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
-# do
-#     for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
-#     do
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numu${energy}_cc_c5arewto_ma${ma}_c5a12.root" \
-#             -p pion_C5A 1.2 &
+for energy in $(seq $ENERGY_MIN $ENERGY_STEP $ENERGY_MAX)
+do
+    for ma in $(seq $MA_MIN $MA_STEP $MA_MAX)
+    do
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_cc_c5arewto_ma${ma}_c5a12.root" \
+            -p pion_C5A 1.2 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numu${energy}_nc_c5arewto_ma${ma}_c5a12.root" \
-#             -p pion_C5A 1.2 &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_nc_c5arewto_ma${ma}_c5a12.root" \
+            -p pion_C5A 1.2 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numubar${energy}_cc_c5arewto_ma${ma}_c5a12.root" \
-#             -p pion_C5A 1.2 &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_cc_c5arewto_ma${ma}_c5a12.root" \
+            -p pion_C5A 1.2 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numubar${energy}_nc_c5arewto_ma${ma}_c5a12.root" \
-#             -p pion_C5A 1.2 &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_nc_c5arewto_ma${ma}_c5a12.root" \
+            -p pion_C5A 1.2 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numu${energy}_cc_c5arewto_ma${ma}_c5a11.root" \
-#             -p pion_C5A 1.1 &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_cc_c5arewto_ma${ma}_c5a11.root" \
+            -p pion_C5A 1.1 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numu${energy}_nc_c5arewto_ma${ma}_c5a11.root" \
-#             -p pion_C5A 1.1 &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_nc_c5arewto_ma${ma}_c5a11.root" \
+            -p pion_C5A 1.1 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numubar${energy}_cc_c5arewto_ma${ma}_c5a11.root" \
-#             -p pion_C5A 1.1 &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_cc_c5arewto_ma${ma}_c5a11.root" \
+            -p pion_C5A 1.1 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numubar${energy}_nc_c5arewto_ma${ma}_c5a11.root" \
-#             -p pion_C5A 1.1 &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_nc_c5arewto_ma${ma}_c5a11.root" \
+            -p pion_C5A 1.1 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numu${energy}_cc_c5arewto_ma${ma}_c5a13.root" \
-#             -p pion_C5A 1.3 &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_cc_c5arewto_ma${ma}_c5a13.root" \
+            -p pion_C5A 1.3 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numu${energy}_nc_c5arewto_ma${ma}_c5a13.root" \
-#             -p pion_C5A 1.3 &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numu${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numu${energy}_nc_c5arewto_ma${ma}_c5a13.root" \
+            -p pion_C5A 1.3 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numubar${energy}_cc_c5arewto_ma${ma}_c5a13.root" \
-#             -p pion_C5A 1.3 &
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_cc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_cc_c5arewto_ma${ma}_c5a13.root" \
+            -p pion_C5A 1.3 &
 
-#         ../../../bin/reweight_to \
-#             "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" \
-#             -o "${OUTDIR}/numubar${energy}_nc_rewto_ma${ma}_c5a13.root" \
-#             -p pion_C5A 1.3
-#     done
-# done
+        ../../../bin/reweight_to \
+            "${OUTDIR}/numubar${energy}_nc_ma${ma}_c5a12.root" \
+            -o "${OUTDIR}/numubar${energy}_nc_rewto_ma${ma}_c5a13.root" \
+            -p pion_C5A 1.3
+    done
+done
 
 ###### RUN MACRO ######
 
 cd $OUTDIR
 
-for file in *marewto*.root
+for file in *.root
 do
     name=`echo "$file" | cut -d'.' -f1`
     ../../../../bin/myroot -b -q 'res.c("'$name'")'
