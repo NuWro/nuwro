5669a4888550cb87b1620a925e8bf41b3b467a62
Chris Thorpe <cthorpe@pyb044000015.lancs.ac.uk>
2019-11-08 09:30:24 +0000

    Updated hyperon cascade

--- src/hyperon_cascade.h
+++ src/hyperon_cascade.h
@@ -7,4 +7,6 @@ void hyperon_exp_xsec(double E, double Plab, double sigma[], int hyp_state);
 
 void hyperon_state(int hyp_state, double sigma[], int &ij, particle p[]);
 
+double phase_space(double rs, double M1a, double M2a, double M1b, double M2b);
+
 #endif
