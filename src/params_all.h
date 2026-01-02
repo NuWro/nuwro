0e62d4cb76354ec5619771bd0cb9d117ea699818
jsobczyk <jan.sobczyk@uwr.edu.pl>
2026-01-02 10:52:07 +0100

    Change of default value of strength of nucleon cascade (see e-Print: 2512.23350 [hep-ph]

--- src/params_all.h
+++ src/params_all.h
@@ -133,7 +133,7 @@ PARAM(double,tau,8)\
 PARAM(double,formation_length,1)\
 PARAM(bool,first_step,1)\
 PARAM(double,step,0.2)\
-PARAM(double,kaskada_NN_mfp_scale,1)\
+PARAM(double,kaskada_NN_mfp_scale,0.76)\
 PARAM(int,kaskada_NN_xsec,2)\
 PARAM(int,kaskada_NN_inel,2)\
 PARAM(int,kaskada_NN_angle,3)\
